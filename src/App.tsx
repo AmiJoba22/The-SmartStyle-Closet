8230ca6958f37b841fab648082613e3280931c75
AmiJoba22 <aminajobarteh40@gmail.com>
2026-08-29 16:09:52 -0400

    push title to center

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,11 @@ const[count, setCount] = useState(0)
 return (
   <div className='main-app bg-[#FCFBF4]'>
 
-    <div className='hero-section bg-[#FCFBF4] justify-items-center p-15' >
+    <div className='hero-section bg-[#FCFBF4] justify-items-center p-20' >
         <h1 className='font-dot color-brand-olive text-6xl'> The SmartStyle Closet </h1>
-        <p>Here to make fashion and styling choices simpler on any day of the week.</p>
-
+        <p>Here to make fashion and styling choices simpler on any day of the week.    </p>
       <button className='first-button font-dot bg-[#723480] text-white rounded-[20px] p-4 
-      border-2 border-black px-10 justify-items-center'>
+      border-2 border-black px-10'>
         Get Started
       </button>
     </div>
@@ -20,7 +19,7 @@ return (
 
        <div className="flex gap-4 overflow-hidden w-full relative box-border">
       <div className="flex gap-4 w-max animate-[scroll-gallery_30s_linear_infinite]">
-        <img className="w-[200px] h-auto rounded-[10px] shrink-0" src="/assets/style_img_1.png" alt="Style 1" />
+        <img className="w-[100px] h-auto rounded-[10px] shrink-0" src="/assets/style_img_1.png" alt="Style 1" />
         <img className="w-[200px] h-auto rounded-[10px] shrink-0" src="/assets/style_img_2.png" alt="Style 2" />
         <img className="w-[200px] h-auto rounded-[10px] shrink-0" src="/assets/style_img_3.png" alt="Style 3" />
         <img className="w-[200px] h-auto rounded-[10px] shrink-0" src="/assets/style_img_4.png" alt="Style 4" />
