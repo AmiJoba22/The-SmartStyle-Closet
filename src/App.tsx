8911510b551b46eb2c7c83e97d673858223f65e3
AmiJoba22 <aminajobarteh40@gmail.com>
2026-09-21 20:22:56 -0400

    centered button

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ const[count, setCount] = useState(0)
 return (
   <div className='main-app bg-[#FCFBF4]'>
 
-    <div className='hero-section bg-[#FCFBF4] justify-items-center p-20' >
-        <h1 className='font-dot color-brand-olive text-6xl'> The SmartStyle Closet </h1>
-        <p>Here to make fashion and styling choices simpler on any day of the week.    </p>
+    <div className='hero-section bg-[#FCFBF4] flex flex-col items-center p-20' >
+        <h1 className='font-dot color-brand-olive text-6xl py-3'> The SmartStyle Closet </h1>
+        <p className='py-7'>Here to make fashion and styling choices simpler on any day of the week.    </p>
       <button className='first-button font-dot bg-[#723480] text-white rounded-[20px] p-4 
       border-2 border-black px-10'>
         Get Started
