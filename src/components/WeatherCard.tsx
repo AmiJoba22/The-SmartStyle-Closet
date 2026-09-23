8a3abbfe582596a1cd8b568c03536cb98c2f2c16
AmiJoba22 <aminajobarteh40@gmail.com>
2026-09-22 20:05:47 -0400

    weather card titles

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -10,6 +10,20 @@ return (
 
 <div>
     
+    <div> 
+
+        <h3 className='rounded bg-color-brand-plum text-white font-dot'>Step 1: Weather Analysis</h3>
+
+        <p>Back to Home</p>
+
+
+
+    </div>
+
+
+
+
+
 </div>
 
 
